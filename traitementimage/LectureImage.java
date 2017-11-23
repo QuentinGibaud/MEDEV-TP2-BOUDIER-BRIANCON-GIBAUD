d01068ef20d42c25673b13b2d5d1ed6cfda5b8af
QuentinGibaud <quentgib@gmail.com>
2017-11-23 10:29:18 +0100

    Modification de la classe principale

--- traitementimage/LectureImage.java
+++ traitementimage/LectureImage.java
@@ -23,10 +23,11 @@ public class LectureImage {
 
     /**
      * Function to read a file pgm.
+     *
      * @param nomFichierACharger PGM file
      * @return A 2D matrix representing the image(height, width)
      */
-    public static int[][] lireImage(String nomFichierACharger){
+    public static int[][] lireImage(String nomFichierACharger) {
         BufferedReader fichier;
         String delimiteurs = " ,.;";
         int width, height;
@@ -34,117 +35,112 @@ public class LectureImage {
 
         try {
             fichier = new BufferedReader(new FileReader(nomFichierACharger));
-
+            fichier.readLine();
+            fichier.readLine();
             String line = fichier.readLine();
-            line = fichier.readLine();
             StringTokenizer tokenizer = new StringTokenizer(line, delimiteurs);
 
-            if(tokenizer.hasMoreTokens()){
+            if (tokenizer.hasMoreTokens()) {
                 width = Integer.parseInt(tokenizer.nextToken());
-                if(tokenizer.hasMoreTokens()){
+                if (tokenizer.hasMoreTokens()) {
                     height = Integer.parseInt(tokenizer.nextToken());
 
                     image = new int[height][width];
-                    
+                    fichier.readLine();
                     int nbPixelLu = 0;
                     int nbLigneLue = 0;
-                    line = fichier.readLine();                            
-                    while(nbLigneLue != height){
-                        if(nbPixelLu == width){
+                    line = fichier.readLine();
+                    while (nbLigneLue != height) {
+                        if (nbPixelLu == width) {
                             nbPixelLu = 0;
                             nbLigneLue++;
                         }
                         tokenizer = new StringTokenizer(line, delimiteurs);
-                        
-                        while(tokenizer.hasMoreTokens()){
-                            if(nbPixelLu < width){
+
+                        while (tokenizer.hasMoreTokens()) {
+                            if (nbPixelLu < width) {
                                 image[nbLigneLue][nbPixelLu] = Integer.parseInt(tokenizer.nextToken());
                                 nbPixelLu++;
-                            }
-                            else{
+                            } else {
                                 System.err.println("Not a correct pgm file : Width and file not matching");
                             }
                         }
-                        
+
                     }
-                }
-                else{
+                } else {
                     System.err.println("No width indicator in the pgm file");
                 }
-            }
-            else{
+            } else {
                 System.err.println("No length indicator in the pgm file");
             }
-            
+
         } catch (FileNotFoundException ex) {
             Logger.getLogger(LectureImage.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(LectureImage.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return image;
     }
 
-    
-    public static int maxTab(int[] tab){
-        int max = tab [0];
-        for (int i=0;i<tab.length;i++){
-            if (tab[i]>max){
+    public static int maxTab(int[] tab) {
+        int max = tab[0];
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] > max) {
                 max = tab[i];
-            } 
+            }
         }
         return max;
     }
-    
-    public static void histogramme(String nomFichier) throws IOException{
+
+    public static void histogramme(String nomFichier) throws IOException {
         int[][] matriceImage = lireImage(nomFichier);
         int nbLigne = matriceImage.length;
         int nbColonne = matriceImage[0].length;
         int[] histo = new int[256];
-        for(int i=0;i<nbLigne;i++){
-            for (int j=0;j<nbColonne;j++){
+        for (int i = 0; i < nbLigne; i++) {
+            for (int j = 0; j < nbColonne; j++) {
                 histo[matriceImage[i][j]]++;
             }
         }
-        
+
         int maxHisto = maxTab(histo);
-        int hauteur = maxHisto+2;
+        int hauteur = maxHisto + 2;
         int largeur = 258;
-        
+
         int[][] matriceHisto = new int[hauteur][largeur];
-        
-        matriceHisto[hauteur-1][0]=0;
-        matriceHisto[hauteur-1][largeur-1]=0;
-        for(int i =0;i<largeur;i++){
-            matriceHisto[0][i]=0;
-            if ((i>1) && (i<257)){
-                matriceHisto[hauteur-1][i]=i-1;
+
+        matriceHisto[hauteur - 1][0] = 0;
+        matriceHisto[hauteur - 1][largeur - 1] = 0;
+        for (int i = 0; i < largeur; i++) {
+            matriceHisto[0][i] = 0;
+            if ((i > 1) && (i < 257)) {
+                matriceHisto[hauteur - 1][i] = i - 1;
             }
-            
+
         }
-        
-        for(int j=1;j<largeur-1;j++){
-            for(int i=1;i<hauteur-1;i++){
-                if((i>0)&&(i<(maxHisto-histo[j-1]))){
-                    matriceHisto[i][j]=0;
-                }
-                else {
-                    matriceHisto[i][j]=255;
+
+        for (int j = 1; j < largeur - 1; j++) {
+            for (int i = 1; i < hauteur - 1; i++) {
+                if ((i > 0) && (i < (maxHisto - histo[j - 1]))) {
+                    matriceHisto[i][j] = 0;
+                } else {
+                    matriceHisto[i][j] = 255;
                 }
             }
         }
-        
-        ecritureImage(matriceHisto,"histogramme");
-        
+
+        ecritureImage(matriceHisto, "histogramme");
+
     }
-    
+
     public static void ecritureImage(int[][] matImage, String nomImage) throws IOException {
         BufferedWriter bufferedWriter;
         String nomSauvegarde = nomImage + ".pgm";
-        
+
         //Création du BufferedWriter
         bufferedWriter = new BufferedWriter(new FileWriter(nomSauvegarde));
-        
+
         //On entre les informations de base
         bufferedWriter.write("P2");
         bufferedWriter.newLine();
@@ -156,16 +152,16 @@ public class LectureImage {
         bufferedWriter.newLine();
         bufferedWriter.write(255);
         bufferedWriter.newLine();
-        
+
         //On boucle sur le tableau pour écrire par ligne et par colonne
-        for(int i=0;i<numLigne;i++){
-            int [] ligneEnCours = matImage[i];
-            for(int j=0;j<numColonne;j++){
+        for (int i = 0; i < numLigne; i++) {
+            int[] ligneEnCours = matImage[i];
+            for (int j = 0; j < numColonne; j++) {
                 bufferedWriter.write(ligneEnCours[j] + " ");
             }
             bufferedWriter.newLine();
         }
-        
+
         // Je force l'écriture dans le fichier
         bufferedWriter.flush();
         // Puis je le ferme
--- traitementimage/TraitementImage.java
+++ traitementimage/TraitementImage.java
@@ -5,6 +5,8 @@
  */
 package traitementimage;
 
+import java.io.IOException;
+
 /**
  *
  * @author Quentin GIBAUD, Yann BRIANCON, Justine BOUDIER
@@ -14,7 +16,9 @@ public class TraitementImage {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        int[][] matImage = LectureImage.lireImage("lena.pgm");
+        LectureImage.histogramme("lena.pgm");
     }
     
 }
